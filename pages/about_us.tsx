9aa6738ef837ba7fae5956e85d70e532666e32f0
zahid <zeshaq@gmail.com>
2023-02-26 01:59:30 +0600

    about contact

--- pages/about_us.tsx
+++ pages/about_us.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layout"
 
 
-export default function AboutUsPage() {
+export default function AboutUs() {
     return (
 
 
@@ -24,9 +24,16 @@ export default function AboutUsPage() {
         <div className="flex justify-center items-center h-full">
           <div className="text-center text-white px-6 md:px-12">
             <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
-              About Us <br /><span></span>
+            About Us <br /><span></span>
             </h1>
-            </h1>
+            <button
+              type="button"
+              className="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+            >
+              Get started
+            </button>
           </div>
         </div>
       </div>
